a64843877dcb2cc690f6d1b71b49b0bf78c1db8b
Gyeonghwan Kim <gyeonghwan.dev@gmail.com>
2021-01-13 02:54:57 +0900

    [ADD] Project 1 - 키 입력에 따른 audio 태그 재생

--- 01 - JavaScript Drum Kit/drumKit.js
+++ 01 - JavaScript Drum Kit/drumKit.js
@@ -2,10 +2,20 @@
 function init(){
     window.addEventListener("keydown", (e) => {
         const keyElements = [...document.getElementsByClassName("key")];
+        const audioElements = [...document.getElementsByTagName("audio")];
         
         keyElements.forEach(element => {
             const currentDataKey = parseInt(element.getAttribute("data-key"));
-            if(currentDataKey == e.key.charCodeAt(0) - 32) element.classList.add("playing");
+            if(currentDataKey == e.key.charCodeAt(0) - 32) {
+                element.classList.add("playing");
+            }
+        });
+
+        audioElements.forEach(element => {
+            const currentDataKey = parseInt(element.getAttribute("data-key"));
+            if(currentDataKey == e.key.charCodeAt(0) - 32) {
+                element.play();
+            }
         });
         
     });
