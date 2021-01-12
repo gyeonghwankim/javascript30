232d2ce831d35b72c7476e513c1dba97b9596164
Gyeonghwan Kim <gyeonghwan.dev@gmail.com>
2021-01-13 02:36:40 +0900

    [ADD] Project 1 - 키 입력에 따른 key 클래스 하이라이팅
    window.addEventListener에서 keydown과 keyup 이벤트 발생 시 playing 클래스 추가하거나 삭제하여 하이라이팅 효과 발생

--- 01 - JavaScript Drum Kit/drumKit.js
+++ 01 - JavaScript Drum Kit/drumKit.js
@@ -1,6 +1,24 @@
 
-
 function init(){
+    window.addEventListener("keydown", (e) => {
+        const keyElements = [...document.getElementsByClassName("key")];
+        
+        keyElements.forEach(element => {
+            const currentDataKey = parseInt(element.getAttribute("data-key"));
+            if(currentDataKey == e.key.charCodeAt(0) - 32) element.classList.add("playing");
+        });
+        
+    });
+
+    window.addEventListener("keyup", (e) =>{
+        const keyElements = [...document.getElementsByClassName("key")];
+        
+        keyElements.forEach(element => {
+            const currentDataKey = parseInt(element.getAttribute("data-key"));
+            if(currentDataKey == e.key.charCodeAt(0) - 32) element.classList.remove("playing");
+        });
+        
+    });
 
 }
 
